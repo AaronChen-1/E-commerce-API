f205384ba9f588181eaec8e43552bf87967e0ad8
arone415 <105476658+arone415@users.noreply.github.com>
2025-12-15 14:14:16 -0800

    added annotations to make this a REST controller added annotation to make /categories end point added annotation to allow cross site origin requests

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -1,34 +1,41 @@
 package org.yearup.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.CategoryDao;
 import org.yearup.data.ProductDao;
 import org.yearup.models.Category;
 import org.yearup.models.Product;
-
 import java.util.List;
 
-// add the annotations to make this a REST controller
-// add the annotation to make this controller the endpoint for the following url
+@RestController// add the annotations to make this a REST controller
+@RequestMapping("/categories")// add the annotation to make this controller the endpoint for the following url
     // http://localhost:8080/categories
-// add annotation to allow cross site origin requests
+@CrossOrigin// add annotation to allow cross site origin requests
 public class CategoriesController
 {
     private CategoryDao categoryDao;
     private ProductDao productDao;
 
 
-    // create an Autowired controller to inject the categoryDao and ProductDao
-
-    // add the appropriate annotation for a get action
-    public List<Category> getAll()
-    {
-        // find and return all categories
-        return null;
+    @Autowired// create an Autowired controller to inject the categoryDao and ProductDao
+    public CategoriesController(CategoryDao categoryDao, ProductDao productDao) {
+        this.categoryDao = categoryDao;
+        this.productDao = productDao;
     }
 
+    @GetMapping// add the appropriate annotation for a get action
+    @PreAuthorize("permitAll()")
+    public List<Category> getAll() {
+        try {
+            return categoryDao.getAllCategories();
+        } catch (Exception ex) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
+        }
+    }
     // add the appropriate annotation for a get action
     public Category getById(@PathVariable int id)
     {

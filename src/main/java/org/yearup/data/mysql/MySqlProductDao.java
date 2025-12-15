aac5b5f631265e638abdc9994c5b92ec482df0a8
arone415 <105476658+arone415@users.noreply.github.com>
2025-12-15 14:02:06 -0800

    Fixed issure where SQL WHERE clause doesnt check minPrice properly. Only checks maxPrice, ignoring minPrice filter entirely

--- src/main/java/org/yearup/data/mysql/MySqlProductDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlProductDao.java
@@ -25,7 +25,8 @@ public class MySqlProductDao extends MySqlDaoBase implements ProductDao
 
         String sql = "SELECT * FROM products " +
                 "WHERE (category_id = ? OR ? = -1) " +
-                "   AND (price <= ? OR ? = -1) " +
+                "   AND (price >= ? OR ? = -1) " + //Now checks >= for minPrice
+                "   AND (price <= ? OR ? = -1) " + // <= for maxPrice
                 "   AND (subcategory = ? OR ? = '') ";
 
         categoryId = categoryId == null ? -1 : categoryId;
